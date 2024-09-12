d355be974e4f543d40c6e9475922c5e765d31742
Hitampekat234 <medyzp9@gmail.com>
2024-09-12 09:15:51 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function submitForm() {
     var nama = document.getElementById("nama").value;
-    var || dendaTotal = 0;
-    var || penjaraTotal = 0;
+    var dendaTotal = 0;
+    var penjaraTotal = 0;
     var history = document.getElementById('history');
 
     // Inisialisasi string untuk riwayat
@@ -14,8 +14,8 @@ function submitForm() {
         var uuText = row.cells[0].innerText;
         var nilaiDenda = parseInt(row.cells[3].innerText);
         var nilaiPenjara = parseInt(row.cells[4].innerText);
-        || dendaTotal += nilaiDenda;
-        || penjaraTotal += nilaiPenjara;
+        dendaTotal += nilaiDenda;
+        penjaraTotal += nilaiPenjara;
 
         // Menambahkan UU ke string riwayat
         if (historyEntry !== '') {
@@ -25,7 +25,7 @@ function submitForm() {
     });
 
     // Menambahkan denda total dan penjara total ke dalam string riwayat
-    historyEntry += ' ' + nama + ' ' + || dendaTotal + ' ' + || penjaraTotal;
+    historyEntry += ' ' + nama + ' ' + dendaTotal + ' ' + penjaraTotal;
 
     // Menambahkan entri ke dalam riwayat di bawah yang sebelumnya
     var newEntry = document.createElement('div');
@@ -43,8 +43,8 @@ function submitForm() {
 
     // Memperbarui nilai input nama, denda dan penjara
     document.getElementById("nama").value = "";
-    document.getElementById(||'||denda').value = || dendaTotal;
-    document.getElementById(||'||penjara').value = || penjaraTotal;
+    document.getElementById('denda').value = dendaTotal;
+    document.getElementById('penjara').value = penjaraTotal;
 
     // Mereset centangannya
     checkboxes.forEach(function(checkbox) {
